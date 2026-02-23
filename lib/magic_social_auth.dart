9335e437a537fc436a947df24555a5e76acabecd
Anilcan Cakir <anilcan.cakir@gmail.com>
2026-02-23 17:13:26 +0300

    refactor: replace barrel export with magic_social_auth.dart

---
+++ lib/magic_social_auth.dart
@@ -0,0 +1,26 @@
+library magic_social_auth;
+
+// Facade
+export 'src/facades/social_auth.dart';
+
+// Core
+export 'src/social_auth_manager.dart';
+
+// Contracts
+export 'src/contracts/social_driver.dart';
+export 'src/contracts/social_auth_handler.dart';
+
+// Drivers
+export 'src/drivers/google_driver.dart';
+export 'src/drivers/microsoft_driver.dart';
+export 'src/drivers/github_driver.dart';
+
+// Providers
+export 'src/providers/social_auth_service_provider.dart';
+
+// Models
+export 'src/models/social_token.dart';
+export 'src/models/social_platform.dart';
+
+// Exceptions
+export 'src/exceptions/social_auth_exception.dart';
